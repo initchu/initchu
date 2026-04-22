c608a8c7b711b9c5925de6c6055c719dfd445556
GitHub Action <action@github.com>
2026-04-22 16:05:35 +0800

    Revert "update commit"
    This reverts commit 1c22d2bd22a6398130b015c4278649f82326360f.

--- update-stats.js
+++ update-stats.js
@@ -12,16 +12,12 @@ fs.readFile('README.md', 'utf8', (err, data) => {
 
   const updatedContent = data
     .replace(
-      /https:\/\/github-readme-stats\.vercel\.app\/api\?[^"')]+/g,
-      `https://github-readme-stats.vercel.app/api?username=${GITHUB_USERNAME}&show_icons=true&count_private=true&icon_color=00b3ff&theme=blue-green&title_color=00b3ff&hide_border=true&include_all_commits=true&v=${timestamp}`
+      /https:\/\/github-readme-stats-sigma-five\.vercel\.app\/api\?[^"')]+/g,
+      `https://github-readme-stats-sigma-five.vercel.app/api?username=${GITHUB_USERNAME}&show_icons=true&count_private=true&icon_color=00b3ff&theme=blue-green&title_color=00b3ff&hide_border=true&include_all_commits=true&v=${timestamp}`
     )
     .replace(
-      /https:\/\/github-readme-stats\.vercel\.app\/api\/top-langs\/\?[^"')]+/g,
-      `https://github-readme-stats.vercel.app/api/top-langs/?username=${GITHUB_USERNAME}&layout=compact&theme=blue-green&title_color=00b3ff&hide_border=true&langs_count=8&v=${timestamp}`
-    )
-    .replace(
-      /https:\/\/streak-stats\.demolab\.com\/\?[^"')]+/g,
-      `https://streak-stats.demolab.com/?user=${GITHUB_USERNAME}&theme=blue-green&title_color=00b3ff&hide_border=true&v=${timestamp}`
+      /https:\/\/github-readme-streak-stats\.herokuapp\.com\/\?[^"')]+/g,
+      `https://streak-stats.demolab.com/?user=${GITHUB_USERNAME}&count_private=true&theme=blue-green&title_color=00b3ff&hide_border=true&v=${timestamp}`
     );
 
   fs.writeFile('README.md', updatedContent, 'utf8', (err) => {
